9f0bfda16a1c80a01cfbb53974bb8e5d10da339b
Teut2711 <manglavishesh64@gmail.com>
2024-09-28 05:20:17 +0530

    feat: added icon

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/chemical_manager/" : "/",
-  plugins: [VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: [
+        {
+          icons: [{ type: "image/png", name: "chemical-flask.png" , size:"512X512"}],
+        },
+      ],
+    }),
+  ],
 });
