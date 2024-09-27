fe4c9979e8b2a300c397b220958e07b6c090520f
Teut2711 <manglavishesh64@gmail.com>
2024-09-28 04:51:38 +0530

    feat: add pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/chemical_manager/" : "/",
+  plugins: [VitePWA({ registerType: "autoUpdate" })],
 });
