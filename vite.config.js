b193d3ad4943b6ab22a901d675db434c853c561b
Teut2711 <manglavishesh64@gmail.com>
2024-09-28 06:05:16 +0530

    fix: table overflow

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,7 @@ export default defineConfig({
         ],
         screenshots: [
           {
-            form_factor: "wide",
-
+ 
             src: "chemical-flask.png",
             sizes: "512X512",
             type: "image/png",
